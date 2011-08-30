858d1f376f5b546eecebdbd33e84bf3254be1f62
Adam Venturella <aventurella@gmail.com>
2011-08-29 21:12:48 -0700

    stuff

---
+++ crazyhorse/features/requests/form_parser.py
@@ -0,0 +1,31 @@
+import urlparse
+from crazyhorse.features.requests.mime_multipart import parse_mime_multipart
+class FormParser(object):
+
+    def __init__(self):
+        self.files = None
+
+    def parse_body(self, content_type, length, data):
+        if length > 0:
+
+            if content_type.startswith("application/x-www-form-urlencoded"):
+                return self._parse_urlencoded(length, data)
+
+            elif content_type.startswith("multipart/form-data"):
+                # boundary= is at the end of the content_type, so
+                # start looking for the 1st = from the end of the string
+                index      = content_type.rfind("=") + 1
+                boundary   = content_type[index:]
+                params     = self._parse_multipart(length, data, boundary)
+                self.files = params["files"]
+                return params["data"]
+
+        return {}
+
+    def _parse_urlencoded(self, length, data):
+        body = data.read(length)
+        return urlparse.parse_qs(body)
+
+    def _parse_multipart(self, length, data, boundary):
+        body   = data.read(length)
+        return parse_mime_multipart(boundary, body)
---
+++ crazyhorse/features/requests/forms.py
@@ -0,0 +1,26 @@
+from crazyhorse.features.requests.form_parser import FormParser
+from crazyhorse.features.params import ParamCollection
+
+def feature_forms(context):
+    content_length = -1
+
+    try:
+        content_length = int(context.environ.get("CONTENT_LENGTH", "0"))
+    except ValueError:
+        pass
+
+    if content_length > 0:
+            parser = FormParser()
+            values = {"content_type":context.environ.get("CONTENT_TYPE", "application/unknown"),
+                      "length": content_length,
+                      "data":context.environ["wsgi.input"]}
+
+            # merge a dictionary:
+            # http://stackoverflow.com/questions/38987/how-can-i-merge-two-python-dictionaries-as-a-single-expression
+
+            params = ParamCollection(parser.parse_body(**values))
+            context.request.params = params
+
+            # TODO handle files
+            #if len(parser.files) > 0:
+            #    self.files.update(parser.files)
